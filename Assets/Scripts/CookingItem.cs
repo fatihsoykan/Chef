712206e41b4ebd4c0d4433ca14ddeca0145a0f3b
fatihsoykan <infofatihsoykan@gmail.com>
2025-07-11 09:18:27 +0300

    v0.01 Sprites Added
    canvas ayarlandı
    sprite'lar eklendi.
    ilk script yazılacak

---
+++ Assets/Scripts/CookingItem.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
