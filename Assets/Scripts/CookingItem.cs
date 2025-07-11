9d50cbe8f7ef26249867898c247cdd7d0a50381d
fatihsoykan <infofatihsoykan@gmail.com>
2025-07-11 22:49:24 +0300

    v0.02 draggin code added

--- Assets/Scripts/CookingItem.cs
+++ Assets/Scripts/CookingItem.cs
@@ -1,8 +1,39 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class CookingItem : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
+    //state 1: dragging
+    //state 2: not dragging
+    bool dragging =false;
+    Vector2 oldPosition;
+    RectTransform rectTransform;
+    
+    public void OnPointerDown(PointerEventData eventData) //hangi nesneye týklarsak o nesnede çalýþýr, script diðer nesnelerde olsada çalýþmaz.
+    {
+        //mouse ile týklandýðýnda çalýþan özel bir komut
+
+        transform.SetAsLastSibling(); //týkladýðýmýz nesneyi hiyerarþide en alta alýr böylece ekranda en üstte görürüz.
+        oldPosition = rectTransform.anchoredPosition;
+        dragging = true;
+       
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //mouse ile týklamayý býraktýðýnýzda çalýþan özel bir komut
+        rectTransform.anchoredPosition = oldPosition;
+        dragging = false;
+    }
+    
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+     
+
+    }
 
 
     private void Start()
@@ -12,10 +43,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
 
     private void Update()
-    {
-        
+    {     
+        if (dragging)
+        {
+            //drag the item          
+            rectTransform.anchoredPosition = Input.mousePosition;
+            Debug.Log("týklandý");
+        }
     }
-
+    
 
 
 
